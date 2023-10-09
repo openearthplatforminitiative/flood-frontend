cc7bf4f875da4c5732df6d51aca3f5d5051c7979
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2023-10-09 12:16:07 +0200

    Cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import {ReactNode} from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en">
