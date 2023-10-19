ff78277782fa6ba37942f652014c47c136a7a4ec
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2023-10-19 14:47:21 +0200

    Removed old unused i18n code

--- app/[lang]/dictionaries.ts
+++ app/[lang]/dictionaries.ts
@@ -30,25 +30,3 @@ const getFromDictionary = (
   const key = keys.shift() || '';
   return getFromDictionary(keys, dict[key]);
 };
-export const _t = (key: string, dict: Dict): string => {
-  if (!key) {
-    return '';
-  }
-  const keys = key.split('.');
-  const ret = getFromDictionary(keys, dict);
-
-  if (!ret) {
-    return key;
-  }
-  if (typeof ret !== 'string') {
-    console.error('getFromDict returner a ', typeof ret);
-    return key;
-  }
-
-  return ret;
-};
-
-export const getTranslations = async (lang: Lang): Promise<Translator> => {
-  const dict = await getDictionary(lang);
-  return (key: string) => _t(key, dict);
-};
