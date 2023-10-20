97e29aacf65c93f50e75ad754ea2d0839e81d730
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2023-10-20 16:34:57 +0200

    Added placeholders for text and buttons

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,7 +3,11 @@ import Dashboard from '@/app/[lang]/pages/dashboard/page';
 
 const Home = async ({ params: { lang } }: { params: { lang: string } }) => {
   return (
-    <Box width={'100%'} height={'100%'}>
+    <Box
+      width={'100%'}
+      height={'100%'}
+      style={{ display: 'flex', justifyContent: 'center' }}
+    >
       <Dashboard lang={lang} />
     </Box>
   );
--- app/[lang]/pages/onboarding-welcome/page.tsx
+++ app/[lang]/pages/onboarding-welcome/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import bg from '@/public/assets/images/start-screen-image.png';
 import Title from '@/app/[lang]/components/Title';
 
@@ -21,7 +21,33 @@ const OnboardingWelcome = () => {
       >
         <Title />
       </Box>
-      <Box style={{ backgroundColor: 'white' }}>Some text</Box>
+      <Box
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          backgroundColor: 'white',
+          padding: '30px',
+          marginTop: '15px',
+        }}
+      >
+        <Typography>
+          {' '}
+          Some additional explanatory text here, this is a placeholder.
+        </Typography>
+        <ul>
+          <li>Receive flood warnings</li>
+          <li>Some other point</li>
+          <li>And one final nice thing</li>
+        </ul>
+      </Box>
+      <Box
+        style={{ display: 'flex', flexDirection: 'column', padding: '20px' }}
+      >
+        <Button variant={'contained'}>Create account</Button>
+        <Button variant={'outlined'} style={{ marginTop: '15px' }}>
+          Log in
+        </Button>
+      </Box>
     </Box>
   );
 };
