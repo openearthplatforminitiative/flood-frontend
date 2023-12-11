5a3227ffaf167e9da95d62c1424d11caba085fe9
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2023-12-11 12:55:22 +0100

    Signing in now works using keycloak login page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,15 +2,28 @@ import NextAuth from 'next-auth';
 import KeycloakProvider from 'next-auth/providers/keycloak';
 import { PrismaClient } from '@prisma/client';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { AdapterAccount } from '@auth/core/adapters';
+import { Adapter } from 'next-auth/adapters';
 
 const prisma = new PrismaClient();
 
+const prismaAdapter = PrismaAdapter(prisma);
+
+const MyAdapter: Adapter = {
+  ...prismaAdapter,
+  linkAccount: (account: AdapterAccount) => {
+    account['not_before_policy'] = account['not-before-policy'];
+    delete account['not-before-policy'];
+    return prismaAdapter.linkAccount?.(account);
+  },
+};
+
 const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: MyAdapter,
   providers: [
     KeycloakProvider({
-      clientId: process.env.KEYCLOAK_ID!,
-      clientSecret: process.env.KEYCLOAK_SECRET!,
+      clientId: process.env.KEYCLOAK_ID ?? '',
+      clientSecret: process.env.KEYCLOAK_SECRET ?? '',
       issuer: process.env.KEYCLOAK_ISSUER,
     }),
   ],
