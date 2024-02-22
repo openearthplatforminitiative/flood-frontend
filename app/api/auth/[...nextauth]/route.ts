b297dd0ac21dbd61ea68c18dede595a36d986ec1
Henrik Aspenes Vedal <henrikvedal@hotmail.com>
2024-02-22 09:18:05 +0100

    Removed unused code

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,7 @@
 import NextAuth from 'next-auth';
 import KeycloakProvider from 'next-auth/providers/keycloak';
-import { PrismaClient } from '@prisma/client';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { AdapterAccount } from '@auth/core/adapters';
-import { Adapter } from 'next-auth/adapters';
-
-const prisma = new PrismaClient();
-
-const prismaAdapter = PrismaAdapter(prisma);
-
-const MyAdapter: Adapter = {
-  ...prismaAdapter,
-  linkAccount: (account: AdapterAccount) => {
-    account['not_before_policy'] = account['not-before-policy'];
-    delete account['not-before-policy'];
-    return prismaAdapter.linkAccount?.(account);
-  },
-};
 
 const handler = NextAuth({
-  //adapter: MyAdapter,
   providers: [
     KeycloakProvider({
       clientId: process.env.KEYCLOAK_ID ?? '',
