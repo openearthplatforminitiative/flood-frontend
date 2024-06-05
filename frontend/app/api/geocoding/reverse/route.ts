045fe491d14aa07ae5d6e5eaa943768b70789e12
Erik Dymbe <erik.dymbe@gmail.com>
2024-06-05 17:56:15 +0200

    feat: bump dependencies

--- frontend/app/api/geocoding/reverse/route.ts
+++ frontend/app/api/geocoding/reverse/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
   const client = new GeocoderClient();
 
   try {
-    const result = await client.getReverseGeocoding(lon, lat);
+    const result = await client.getReverseGeocoding({ lon, lat });
     const { data } = result;
     return NextResponse.json({
       data: data,
