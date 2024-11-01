7a75b0e46b4674d396f5036542a2700d98aa6662
Sander Krøglid <sander.kroglid@knowit.no>
2024-11-01 12:29:34 +0100

    fix: move marker to current location on site edit

--- frontend/app/components/onboarding/SiteMap.tsx
+++ frontend/app/components/onboarding/SiteMap.tsx
@@ -2,19 +2,23 @@
 
 import { Circle, MapContainer, Marker, TileLayer, useMap } from 'react-leaflet';
 import { useEffect } from 'react';
-import { DragEndEvent, LatLng, LocationEvent } from 'leaflet';
+import { DragEndEvent, Icon, LatLng, LocationEvent } from 'leaflet';
 import LocateControl from '@/app/components/onboarding/LocateControl';
 
-interface SiteMapProps {
-  lat: number;
-  lng: number;
-  radius: number;
+type SiteMapProps = {
+  lat?: number;
+  lng?: number;
+  radius?: number;
   setRadius: (value: number) => void;
   setLat: (value: number) => void;
   setLng: (value: number) => void;
-}
+};
 
 const SiteMap = ({ lat, lng, radius, setLat, setLng }: SiteMapProps) => {
+  lat ??= 51.505;
+  lng ??= -0.09;
+  radius ??= 0;
+
   const LocationMarker = () => {
     const map = useMap();
 
@@ -49,15 +53,15 @@ const SiteMap = ({ lat, lng, radius, setLat, setLng }: SiteMapProps) => {
 
   return (
     <MapContainer
-      center={{ lat: 51.505, lng: -0.09 }}
-      zoom={14}
-      scrollWheelZoom={false}
-      style={{ width: '312px', height: '320px', flexShrink: '0' }}
+      center={{ lat, lng }}
+      zoom={5}
+      scrollWheelZoom={true}
+      style={{ width: '512px', height: '512px', flexShrink: '0' }}
       attributionControl={false}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=yKbUfk8FeU9lYSvB92QQ"
       />
       <LocationMarker />
       <LocateControl
